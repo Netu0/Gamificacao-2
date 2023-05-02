3a2ae17c665033a5ee13741a9bfe638467d62dc0
Isa <osowskiisabela@gmail.com>
2023-05-02 10:39:14 -0300

    listar produtos

--- Gameficacao2/UI/ProdutoUI.cs
+++ Gameficacao2/UI/ProdutoUI.cs
@@ -30,6 +30,9 @@ namespace eRoupas.UI
                         break;
                     case 3:
                         ListarProduto();
+                        ListarCamisa();
+                        ListarCalca();
+                        ListarVestido();
                         break;
                     case 0:
                         Console.WriteLine("Retornando ...");
@@ -91,6 +94,39 @@ namespace eRoupas.UI
             foreach (var produto in _produtos)
             {
                 Console.WriteLine($"Nome: {produto.Nome} | Preco: {produto.Preco} ");
+            }
+        }
+
+        private void ListarCamisa()
+        {
+            Console.WriteLine("Lista de camisas:");
+            foreach (var produto in _produtos)
+            {
+                if (produto is Camisa camisa){
+                    Console.WriteLine($"Nome: {camisa.Nome} | Preco: {camisa.Preco} | Cor: {camisa.Cor} | Tamanho: {camisa.Tamanho} | Marca: {camisa.Marca}");
+                }
+            }
+        }
+
+        private void ListarCalca()
+        {
+            Console.WriteLine("Lista de calças:");
+            foreach (var produto in _produtos)
+            {
+                if (produto is Calca calca){
+                    Console.WriteLine($"Nome: {calca.Nome} | Preco: {calca.Preco} | Cor: {calca.Cor} | Tamanho: {calca.Tamanho} | Marca: {calca.Marca}");
+                }
+            }
+        }
+
+        private void ListarVestido()
+        {
+            Console.WriteLine("Lista de vestidos:");
+            foreach (var produto in _produtos)
+            {
+                if (produto is Vestido vestido){
+                    Console.WriteLine($"Nome: {vestido.Nome} | Preco: {vestido.Preco} | Cor: {vestido.Cor} | Tamanho: {vestido.Tamanho} | Marca: {vestido.Marca}");
+                }
             }
         }
     }
